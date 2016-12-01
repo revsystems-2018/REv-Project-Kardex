97919dece72ffbf4841fe21389d69cc31dc040f3
REV Systems <ricardo.veizaga-23-2018@live.com>
2016-12-01 17:58:55 -0400

    Report User v1

--- Proyect_Kardex/ReporteUsuarios.cs
+++ Proyect_Kardex/ReporteUsuarios.cs
@@ -29,7 +29,7 @@ namespace Proyect_Kardex
         {
             InitializeComponent();
             Conectar();
-            toolTip1.SetToolTip(chart1, "Grafico de Barras de los Usuarios Cantidad y Pagos en Ventas");
+            toolTip1.SetToolTip(chart1, "Grafico de Barras de los Usuarios Cantidad y Efectivo en Ventas");
             toolTip1.SetToolTip(chartorta, "Grafico de Pastel de Pagos en Ventas Realizadas");
             toolTip1.SetToolTip(dataprodgrid, "Datos de los Usuarios");
         }
@@ -45,7 +45,8 @@ namespace Proyect_Kardex
         private void ReporteUsuarios_Load(object sender, EventArgs e)
         {
             String lee = "SELECT name_Cliente AS Nombre, SUM(num_Prod) AS Cantidad, SUM(pago_Cliente) AS Efectivo_Compras FROM REV_Ventas GROUP BY name_Cliente; ";
-
+            String mes = "SELECT name_User AS Nombre_Usuario, SUM (pago_Cliente) AS Efectivo_En_Ventas, SUM (num_Prod) AS Cantidad FROM REV_Ventas WHERE DATEPART (mm, Fecha_Venta) = (DATEPART (mm, GETDATE())-2)  GROUP BY name_User;";  
+        
             dataprodgrid.DataSource = CargarDatos(lee);
             chart1.DataSource = CargarDatos(lee);
             chart1.Series["Series1"].LegendText = "Productos";
